7ace3f49d0bd8f71312f1664c5be13c0411c8afd
Karim Elaskary <karimelaskarey88@gmail.com>
2024-03-11 14:26:06 +0200

    Api Link Edited

--- src/api.jsx
+++ src/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const getTime = async (city) => {
   const response = await axios.get(
-    'http://api.aladhan.com/v1/timingsByCity?/',
+    'https://api.aladhan.com/v1/timingsByCity?/',
     {
       params: {
         country: 'Egypt',
